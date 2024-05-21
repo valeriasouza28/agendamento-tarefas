1e4a076bcf267572d14c17ddcbab45b6f35dc489
valeriasouza28 <valeriasouza28@gmail.com>
2024-05-21 19:38:35 -0300

    add gitignore

--- obj/Debug/net8.0/Tarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d50ce6b49378ff851a0a6d11895fd8d762f9d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ae394d396ca57750b9431daf368e1624b6ef4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
